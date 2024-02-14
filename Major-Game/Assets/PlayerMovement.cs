b79949cbd8a82325e07c9dcf869fdd353618bfe0
krishpatel2002 <krishjpatel15@gmail.com>
2024-02-14 02:29:58 -0500

    added_movement
    Added jump and moving movements

--- Major-Game/Assets/PlayerMovement.cs
+++ Major-Game/Assets/PlayerMovement.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    private void Update()
+    { 
+        float dirx = Input.GetAxisRaw("Horizontal"); 
+        rb.velocity = new Vector2(dirx * 7f, rb.velocity.y) ;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
+        
         
     }
 }
