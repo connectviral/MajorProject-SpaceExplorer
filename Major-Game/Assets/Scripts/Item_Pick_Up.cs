28c43af43240da5e4a7309b0c08a44ef637d8fb7
RushS09 <rsolanki@confederationcollege.ca>
2024-03-02 13:06:32 -0500

    Added Collectibles
    Added bullets as collectibles and a text which displays the number of bullets collected. The goal is to have those bullets available to the player to shoot down enemies.

---
+++ Major-Game/Assets/Scripts/Item_Pick_Up.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item_Pick_Up : MonoBehaviour
+{
+    private int bullets = 0;
+    [SerializeField] private Text bulletText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("pick_up"))
+        {
+            Destroy(collision.gameObject);
+            bullets++; 
+            bulletText.text = ("Bullets: "+ bullets);
+        }
+    }
+}
